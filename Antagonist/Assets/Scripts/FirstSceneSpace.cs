071ea7d1884f3853afe83b8de2d425f5516c564e
M2neko <bwwang@ucdavis.edu>
2020-08-15 11:33:46 +0800

    Init Scene1

---
+++ Antagonist/Assets/Scripts/FirstSceneSpace.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstSceneSpace : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // bottom
+        if (gameObject.GetComponent<Rigidbody2D>().position.y < -3.35f)
+        {
+            gameObject.GetComponent<Rigidbody2D>().position = new Vector2(gameObject.GetComponent<Rigidbody2D>().position.x, -3.35f);
+        }
+
+
+        // cupboard
+        if (gameObject.GetComponent<Rigidbody2D>().position.x > -4.5f && gameObject.GetComponent<Rigidbody2D>().position.x < 2.92f)
+        {
+            if (gameObject.GetComponent<Rigidbody2D>().position.y > -1.45f)
+            {
+                gameObject.GetComponent<Rigidbody2D>().position = new Vector2(gameObject.GetComponent<Rigidbody2D>().position.x, -1.45f);
+            }
+        }
+
+        // Left
+        if (gameObject.GetComponent<Rigidbody2D>().position.x < -10f)
+        {
+            gameObject.GetComponent<Rigidbody2D>().position = new Vector2(-9.9f, gameObject.GetComponent<Rigidbody2D>().position.y);
+        }
+
+
+        // desk
+        if (gameObject.GetComponent<Rigidbody2D>().position.x > -10.0f && gameObject.GetComponent<Rigidbody2D>().position.x < -7.2f)
+        {
+            if (gameObject.GetComponent<Rigidbody2D>().position.y > -2.05f)
+            {
+                gameObject.GetComponent<Rigidbody2D>().position = new Vector2(gameObject.GetComponent<Rigidbody2D>().position.x, -2.05f);
+            }
+        }
+
+        // chair
+        if (gameObject.GetComponent<Rigidbody2D>().position.x > -7.2f && gameObject.GetComponent<Rigidbody2D>().position.x < -4.5f)
+        {
+            if (gameObject.GetComponent<Rigidbody2D>().position.y > -1.85f)
+            {
+                gameObject.GetComponent<Rigidbody2D>().position = new Vector2(gameObject.GetComponent<Rigidbody2D>().position.x, -1.85f);
+            }
+        }
+
+        // bed
+        if (gameObject.GetComponent<Rigidbody2D>().position.y > -2.8f && gameObject.GetComponent<Rigidbody2D>().position.x > 2.92f && gameObject.GetComponent<Rigidbody2D>().position.x < 9.3f)
+        {
+
+            if (gameObject.GetComponent<Rigidbody2D>().position.x > 2.92f)
+            {
+                gameObject.GetComponent<Rigidbody2D>().position = new Vector2(gameObject.GetComponent<Rigidbody2D>().position.x, -2.8f);
+            }
+
+            if (gameObject.GetComponent<Rigidbody2D>().position.y > -2.8f)
+            {
+                gameObject.GetComponent<Rigidbody2D>().position = new Vector2(2.92f, gameObject.GetComponent<Rigidbody2D>().position.y);
+            }
+        }
+
+        // door
+        if (gameObject.GetComponent<Rigidbody2D>().position.x > 9.3f)
+        {
+            if (gameObject.GetComponent<Rigidbody2D>().position.y > -2f)
+            {
+                gameObject.GetComponent<Rigidbody2D>().position = new Vector2(gameObject.GetComponent<Rigidbody2D>().position.x, -2f);
+            }
+        }
+
+        // Right
+        if (gameObject.GetComponent<Rigidbody2D>().position.x > 10.2f)
+        {
+            gameObject.GetComponent<Rigidbody2D>().position = new Vector2(10.2f, gameObject.GetComponent<Rigidbody2D>().position.y);
+        }
+
+    }
+}
---
+++ Antagonist/Assets/Scripts/Move.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Move : MonoBehaviour
+{
+    private float Speed = 5.0f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+
+        // ------------------- LeftRight ---------------------------------------
+        if (Input.GetAxisRaw("Horizontal") > 0.01)
+        {
+            if (rigidBody != null)
+            {
+                rigidBody.velocity = new Vector2(this.Speed, rigidBody.velocity.y);
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") < -0.01)
+        {
+            if (rigidBody != null)
+            {
+                rigidBody.velocity = new Vector2(-this.Speed, rigidBody.velocity.y);
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+
+        else
+        {
+            if (rigidBody != null)
+            {
+                rigidBody.velocity = new Vector2(0.0f, rigidBody.velocity.y);
+            }
+        }
+
+        // ---------------------------------------------------------------------
+
+        // ---------------------------- UpDown ---------------------------------
+
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical") - 1.0f) <= 0.0f)
+        {
+            if (rigidBody != null)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, this.Speed);
+                gameObject.GetComponent<Animator>().SetBool("Back", true);
+            }
+        }
+
+        else if (Mathf.Abs(Input.GetAxisRaw("Vertical") + 1.0f) <= 0.0f)
+        {
+            if (rigidBody != null)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, -this.Speed);
+                gameObject.GetComponent<Animator>().SetBool("Back", false);
+            }
+        }
+
+        else
+        {
+            if (rigidBody != null)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0.0f);
+            }
+        }
+        // ---------------------------------------------------------------------
+
+        if (rigidBody.velocity.y / 5.0f > 0.01f)
+        {
+            gameObject.GetComponent<Animator>().SetFloat("FrontVelocity", Mathf.Abs(rigidBody.velocity.x / 5.0f) + Mathf.Abs(rigidBody.velocity.y / 5.0f));
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().SetFloat("FrontVelocity", Mathf.Abs(rigidBody.velocity.x / 5.0f));
+            gameObject.GetComponent<Animator>().SetFloat("Velocity", Mathf.Abs(rigidBody.velocity.x / 5.0f) + Mathf.Abs(rigidBody.velocity.y / 5.0f));
+        }
+
+
+
+
+    }
+}
