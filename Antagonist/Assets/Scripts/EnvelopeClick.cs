9796d3d862fed9e615cbc6adeb07acd41b7980bb
M2neko <bwwang@ucdavis.edu>
2020-08-15 14:55:24 +0800

    Update Letter

---
+++ Antagonist/Assets/Scripts/EnvelopeClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvelopeClick : MonoBehaviour
+{
+    [SerializeField] private GameObject letter = null;
+    [SerializeField] private GameObject black = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            letter.GetComponent<SpriteRenderer>().enabled = true;
+            black.GetComponent<SpriteRenderer>().enabled = true;
+
+        }
+    }
+}
---
+++ Antagonist/Assets/Scripts/Suns.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suns : MonoBehaviour
+{
+    bool stopAdd = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = gameObject.GetComponent<SpriteRenderer>().color.r * 255;
+        if (!stopAdd)
+        {
+            x += 0.5f;
+            if (x > 110) stopAdd = true;
+        }
+        if (stopAdd)
+        {
+            x -= 0.5f;
+            if (x < 67) stopAdd = false;
+        }
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(x / 255f,0f,18f / 255f);
+    }
+}
